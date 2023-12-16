9c3eef74e832fed89a822d6f081bd71e6c2ba221
DarthPudge <145842236+DarthPudge@users.noreply.github.com>
2023-12-16 11:01:45 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,52 +6,49 @@ FILE* file1, * file2;
 
 int main() 
 {
-            //Работа с Файлом <>.
+            //ГђГ ГЎГ®ГІГ  Г± Г”Г Г©Г«Г®Г¬ <>.
 //
-    FILE* file;
-    errno_t err;
-
-    // Открытие файла для записи
-    err = fopen_s(&file, "file.txt", "w");
-    if (err != 0) {
-        printf("Error");
-        return err;
+    FILE* file = NULL;
+    fopen_s(&file, "file.txt", "r+");
+    if (!file) {
+        perror("f == NULL!");
+        return 1;
     }
-    // Запись в файл
-    fputs("Пример работы с файлами в C\n", file);
+    // Р—Р°РїРёСЃСЊ РІ С„Р°Р№Р»
+    fputs("Niggers\n", file);
 
-    // Закрытие файла
+    // Р—Р°РєСЂС‹С‚РёРµ С„Р°Р№Р»Р°
     fclose(file);
 
-                // Работа со строками<>.
-    //1) Длина строки <strlen>.
+                // ГђГ ГЎГ®ГІГ  Г±Г® Г±ГІГ°Г®ГЄГ Г¬ГЁ<>.
+    //1) Г„Г«ГЁГ­Г  Г±ГІГ°Г®ГЄГЁ <strlen>.
 //
      
     /*const char str[] = "ISeeDeadPeople";
     size_t length = StrLen(str);
     printf("String length: %zu \n", length);*/
 
-    //2) Сравнение строк <strcmp>
+    //2) Г‘Г°Г ГўГ­ГҐГ­ГЁГҐ Г±ГІГ°Г®ГЄ <strcmp>
 //
     /*char* str1 = "Zxc";
     char* str2 = "Ghoul";
     StrCmp(str1, str2);*/
 
-    //3) Объединение строк <StrCat>
+    //3) ГЋГЎГєГҐГ¤ГЁГ­ГҐГ­ГЁГҐ Г±ГІГ°Г®ГЄ <StrCat>
 // 
     /*char str1[30] = "ZXC";
     char str2[] = "GHOUL";
     StrCat(str1, str2);
     printf("%s\n", str1);*/
 
-    //4) Копирование строк <CopyStr>
+    //4) ГЉГ®ГЇГЁГ°Г®ГўГ Г­ГЁГҐ Г±ГІГ°Г®ГЄ <CopyStr>
 //
     /*char str1[] = "ZXC";
     char str2[100];
     CopyStr(str1, str2);
     printf("%s\n", str2);*/ 
     
-    //5) Первое вхождение <strstr>
+    //5) ГЏГҐГ°ГўГ®ГҐ ГўГµГ®Г¦Г¤ГҐГ­ГЁГҐ <strstr>
 //
     /*char str[] = "I am the best mider, but there is a caveat: I only play on supports";
     printf("%s\n", strstr(str, "I only"));*/
